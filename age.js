59959e7d3386b91b2e28b8202e70c849abf20f1b
mouad <mo3adbrahmi40@gmail.com>
2023-08-19 06:02:54 -0700

    add files

---
+++ age.js
@@ -0,0 +1,11 @@
+let input = document.getElementById('date');
+let btn = document.querySelector('.btn');
+let result = document.querySelector('.res');
+btn.addEventListener('click',function(){
+    let newest = new Date().getTime()
+    let birth = new Date(input.value).getTime()
+    let ageByMsec = newest - birth
+    let ageByYr = ageByMsec / (365*24*3600*1000)
+    let month = (ageByMsec % (365*24*3600*1000))/(30*24*3600*1000)
+    result.textContent=`your age is ${parseInt(ageByYr)} years old ${parseInt(month)} months`
+})
