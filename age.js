f5f5d56ac2ae45e84a7ac16b6501e73d6a021274
mouad <mo3adbrahmi40@gmail.com>
2023-08-19 06:33:46 -0700

    add some change

--- age.js
+++ age.js
@@ -1,3 +1,4 @@
+
 let input = document.getElementById('date');
 let btn = document.querySelector('.btn');
 let result = document.querySelector('.res');
@@ -5,6 +6,8 @@ btn.addEventListener('click',function(){
     let newest = new Date().getTime()
     let birth = new Date(input.value).getTime()
     let ageByMsec = newest - birth
+
+/*we can change this operation*/
     let ageByYr = ageByMsec / (365*24*3600*1000)
     let month = (ageByMsec % (365*24*3600*(10*100))/(30*24*3600*1000)
     result.textContent=`your age is ${parseInt(ageByYr)} years old ${parseInt(month)} months`
